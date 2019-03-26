1da3a1de9b9c9bc1268cedfebae7480dd8ec0761
Brian Arango <brianvictus@gmail.com>
2019-03-26 18:46:47 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,13 @@
-def hello_t(name)
-  name.each {|name| puts "#{name}"}
-end
+#def hello_t(name)
+  #name.each {|name| puts "#{name}"}
+#end
 
-# call your method here!
+def hello_t(array)
+  i = 0 
+  while i < array.length
+  yield(array[i])
+  i = i + 1 
+end
+array
+end
 
