58b760edd81811a33a935f8cbef986f45ad16438
Brian Arango <brianvictus@gmail.com>
2019-03-26 18:42:39 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,7 @@
 def hello_t(name)
   name.each {|name| puts "#{name}"}
   other_names = []
-  if hello_t.start_with? ("A", "J", "P")
+  if hello_t.start_with? ("A,", "J", "P")
     do other_names
   end
 end
